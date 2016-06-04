b9381d5ef6908433553bf47657dfd3e775654824
amaranthine <amaranthine.kt@gmail.com>
2016-06-04 11:50:48 +0900

    gitignore add

---
+++ README.md
@@ -0,0 +1,11 @@
+# Script etc about MacOS Initial Provisioning for me
+
+# Involved
+- Shell Script
+ - for Mac OS Setting
+ - for Software Install about develepment or so
+- Ansible yaml
+
+# Will...
+will replace setting script to ansible yaml
+
---
+++ softs_install.sh
@@ -0,0 +1,30 @@
+# 開発環境のインストール =========================
+#
+# 参考情報
+# 
+# Mac の開発環境構築を自動化する (2015 年初旬編)
+# http://t-wada.hatenablog.jp/entry/mac-provisioning-by-ansible
+
+
+
+# xcode
+sudo xcodebuild -license
+
+
+# Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Homebrew Update
+brew update
+
+# Python install - MacデフォルトのPythonだとAnsibleの動作要件たる2.4以上でない可能性がある
+brew install python
+
+# Ansible install
+brew install ansible
+
+# Ansible用アプリケーションインストール先パス指定
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
+# Ansibleを実行
+ansible-playbook -i hosts ansible_playbook/localhost.yml
